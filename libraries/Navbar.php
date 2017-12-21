98ca5371b3a1b7d36ae9dc5fcd09edec6e5962dd
Unknown <stobenetti@gmail.com>
2017-12-21 15:40:51 -0200

    Parametrização de cor de dropdown para menu de usuário

--- libraries/Navbar.php
+++ libraries/Navbar.php
@@ -126,11 +126,11 @@ class Navbar extends ORMObject{
 
     private function getUserMenu($user = null) {
         $html = '<li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle waves-effect waves-light" id="navbarDropdownMenuLink-4"
+                <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink-4"
                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fa fa-user"></i>';
         $html .= '&nbsp;' . $user->first_name;
         $html .= '</a>
-                <div class="dropdown-menu dropdown-menu-right dropdown-dark" aria-labelledby="navbarDropdownMenuLink-4">
+                <div class="dropdown-menu dropdown-menu-right dropdown-' . $this->dropdownColor . '" aria-labelledby="navbarDropdownMenuLink-4">
                     <a class="dropdown-item waves-effect waves-light" href="<?= base_url(\'/auth/change_password\') ?>"><i
                                 class="fa fa-lock fa-fw"></i>&nbsp;Trocar senha</a>
                     <a class="dropdown-item waves-effect waves-light" href="<?= base_url(\'/auth/logout\') ?>"><i
