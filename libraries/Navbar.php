31c647e7174a2e881f497af30110453911dae1cc
Unknown <stobenetti@gmail.com>
2017-12-22 13:47:35 -0200

    Menu de usuário refatorado (carrega views)

--- libraries/Navbar.php
+++ libraries/Navbar.php
@@ -141,18 +141,16 @@ class Navbar extends ORMObject{
     }
 
     private function getUserMenu($user = null) {
-        $html = '<li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink-4"
-                   data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fa fa-user"></i>';
-        $html .= '&nbsp;' . $user->first_name;
-        $html .= '</a>
-                <div class="dropdown-menu dropdown-menu-right dropdown-' . $this->dropdownColor . '" aria-labelledby="navbarDropdownMenuLink-4">
-                    <a class="dropdown-item waves-effect waves-light" href="<?= base_url(\'/auth/change_password\') ?>"><i
-                                class="fa fa-lock fa-fw"></i>&nbsp;Trocar senha</a>
-                    <a class="dropdown-item waves-effect waves-light" href="<?= base_url(\'/auth/logout\') ?>"><i
-                                class="fa fa-sign-out fa-fw"></i>&nbsp;Sair</a>
-                </div>
-                </li>';
+        $ci = &get_instance();
+        if ($user != null) {
+            $html = $ci->load->view('menu/usermenu', array(
+                'firstName' => $user->first_name,
+                'dropdownColor' => $this->dropdownColor
+            ), true);
+        }
+        else {
+            $html = $ci->load->view('menu/loginButton', null, true);
+        }
         return $html;
     }
 
