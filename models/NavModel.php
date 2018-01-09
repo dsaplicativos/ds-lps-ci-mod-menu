785c48b4a5ea847d923ed57c441f24d02ba5b9d6
Unknown <stobenetti@gmail.com>
2018-01-09 15:32:54 -0200

    Substituição de model por interface

---
+++ models/NavModel.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by Raquel Baldavira on PhpStorm.
+ * DS Aplicativos for fortville
+ * Date: 04/01/2018
+ * Time: 09:25
+ */
+
+interface NavModel {
+
+    public function getHTML($type, $user = null);
+
+}
